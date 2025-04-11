346bb8b47db71bded1c31156ee47035c911506be
Vittorio Palladino <vittorio.palladino@mail.polimi.it>
2025-04-11 19:15:19 +0100

    fixed typo in kernel

--- aie-kernel/lif_kernel.cc
+++ aie-kernel/lif_kernel.cc
@@ -1,92 +1,146 @@
-//===- snn_neuron.cc -------------------------------------------*- C++ -*-===//
+//===- test.cpp -------------------------------------------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-// Copyright (C) 2024, Advanced Micro Devices, Inc.
+// Copyright (C) 2023, Advanced Micro Devices, Inc.
 //
 //===----------------------------------------------------------------------===//
 
-#define NOCPP
-#define UNITARY_POTENTIAL 1
-#define LEAK 0.0f
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <sstream>
 
-#include <stdint.h>
-#include <stdlib.h>
+#include "test_utils.h"
+#include "xrt/xrt_bo.h"
 
-#include <aie_api/aie.hpp>
+#ifndef DATATYPES_USING_DEFINED
+#define DATATYPES_USING_DEFINED
+using DATATYPE = std::uint32_t; // Configure this to match your buffer data type
+#endif
 
-template <int N>
-__attribute__((noinline)) void snnNeuron_aie_integer(int32_t *restrict in, 
-                                            int32_t *restrict out,
-                                            const int32_t threshold,
-                                            const int32_t height,
-                                            const int32_t width) {
-  event0();
+namespace po = boost::program_options;
+const int threshold = 0.5;
 
-  // Since i cannot pipelined multiple sum over the same neuron maybe it is beneficial to implement multiple neurons on the same kernel.
-  // Using 16 elements per vector since we're working with 32-bit integers
-  constexpr int VECTOR_SIZE = 16;
+int main(int argc, const char *argv[]) {
 
-  // Initialize membrane potential
-  int32_t membrane_potential = 0;
+  // Program arguments parsing
+  po::options_description desc("Allowed options");
+  po::variables_map vm;
+  test_utils::add_default_options(desc);
 
-  v16int32 *restrict outPtr = (v16int32 *)out;
-  v16int32 *restrict inPtr = (v16int32 *)in;
+  test_utils::parse_options(argc, argv, desc, vm);
+  int verbosity = vm["verbosity"].as<int>();
 
-  for (int j = 0; j < (height * width); j += VECTOR_SIZE) {
-    chess_prepare_for_pipelining chess_loop_range(6, ) {
-      // Load input spikes
-      v16int32 input_spikes = *inPtr++;
-      v16int32 output_spikes = null_v16int32();
-      
-      // Process each element in the vector
-      // To have a comparison with what has been implemented in snnTorch use a vector of 16 spikes each of them encoded as 32 bit integer.
-      for (int i = 0; i < VECTOR_SIZE; i++) {
+  // Declaring design constants
+  constexpr bool VERIFY = true;
+  constexpr int IN_SIZE = 1024;
+  constexpr int OUT_SIZE = IN_SIZE;
 
-        //Take one element out of the vector
-        int32_t spike = ext_elem(input_spikes, i);  // One 32-bit spike value
 
-        // Update the membrane potential if there is a spike
-        if(spike == 1){
-          membrane_potential += UNITARY_POTENTIAL;
-        }
+  // Load instruction sequence
+  std::vector<uint32_t> instr_v =
+      test_utils::load_instr_binary(vm["instr"].as<std::string>());
 
-        // Apply leak to the membrane potential
-        membrane_potential -= LEAK;
-        
-        //Initiliaze the output to zero and verify if there is a spike or not
-        int32_t output = 0;
-        if (membrane_potential >= threshold) {
-          output = 1;
-          membrane_potential = 0;
-        }
-      
-        output_spikes = upd_elem(output_spikes, i, output);
-      }
-      
-      
-      // Store output vector. outPtr point to a vector of 16 integer elements.
-      *outPtr++ = output_spikes;
+  if (verbosity >= 1)
+    std::cout << "Sequence instr count: " << instr_v.size() << "\n";
+
+  // Start the XRT context and load the kernel
+  xrt::device device;
+  xrt::kernel kernel;
+
+  test_utils::init_xrt_load_kernel(device, kernel, verbosity,
+                                   vm["xclbin"].as<std::string>(),
+                                   vm["kernel"].as<std::string>());
+
+
+  // Setting up the object buffers
+
+  auto bo_instr = xrt::bo(device, instr_v.size() * sizeof(int),
+                          XCL_BO_FLAGS_CACHEABLE, kernel.group_id(1));
+  auto bo_in_spikes = xrt::bo(device, IN_SIZE * sizeof(DATATYPE),
+                        XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(3));
+  auto bo_threshold = xrt::bo(device, 1 * sizeof(DATATYPE),
+                             XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(4));
+  auto bo_out_spikes = xrt::bo(device, OUT_SIZE * sizeof(DATATYPE),
+                         XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(5));
+
+  if (verbosity >= 1)
+    std::cout << "Writing data into buffer objects.\n";
+
+  // Copy instruction stream to xrt buffer object
+  void *bufInstr = bo_instr.map<void *>();
+  memcpy(bufInstr, instr_v.data(), instr_v.size() * sizeof(int));
+
+  // Initialize buffer bo_inA
+  // Initialize with the input spikes
+  DATATYPE *buf_in_spikes = bo_in_spikes.map<DATATYPE *>();
+  
+  //TODO find a better way to generate the input spikes
+  std::mt19937 gen(42); // Fixed seed for reproducibility
+  std::bernoulli_distribution dist(0.1);
+  
+  for (int i = 0; i < NUM_NEURONS; ++n) {
+        buf_in_spikes[i] = dist(gen) ? 1 : 0;
+  }
+
+  //Initialise threshold in input
+  DATATYPE *buf_threshold = bo_threshold.map<uint32_t*>();
+  buf_threshold = threshold;
+
+  //TODO make one buffer only for the parameters (threshold, leak, ...)
+
+  // Zero out buffer for the output spikes
+  DATATYPE *buf_out_spikes = bo_out_spikes.map<DATATYPE *>();
+  memset(bufOut, 0, OUT_SIZE * sizeof(DATATYPE));
+
+  // SYNC HOST TO DEVICE MEMORIES //
+
+  // sync host to device memories
+  bo_instr.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_in_spikes.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_threshold.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_out_spikes.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+
+  // Execute the kernel and wait to finish
+  if (verbosity >= 1)
+    std::cout << "Running Kernel.\n";
+  unsigned int opcode = 3;
+  auto run =
+      kernel(opcode, bo_instr, instr_v.size(), bo_in_spikes, bo_threshold, bo_out_spikes);
+  run.wait();
+
+  // COMPARING RESULT //
+
+  // Build a vector with the output spike (ref) to compare with the bufOut[i]
+  int errors = 0;
+  if (verbosity >= 1) {
+    std::cout << "Verifying results ..." << std::endl;
+  }
+  for (uint32_t i = 0; i < IN_SIZE; i++) {
+    int32_t ref = bufInA[i] * scaleFactor;
+    int32_t test = bufOut[i];
+    if (test != ref) {
+      if (verbosity >= 1)
+        std::cout << "Error in output " << test << " != " << ref << std::endl;
+      errors++;
+    } else {
+      if (verbosity >= 1)
+        std::cout << "Correct output " << test << " == " << ref << std::endl;
     }
   }
 
-    //Compute the time taken by the main kernel
-  event1();
+  // Print Pass/Fail result of our test
+  if (!errors) {
+    std::cout << std::endl << "PASS!" << std::endl << std::endl;
+    return 0;
+  } else {
+    std::cout << std::endl
+              << errors << " mismatches." << std::endl
+              << std::endl;
+    std::cout << std::endl << "fail." << std::endl << std::endl;
+    return 1;
+  }
 }
-
-extern "C" {
-
-void snnNeuronLineInteger(int32_t *in, int32_t *out, int32_t threshold, 
-                  int32_t, int32_t lineWidth) {
-  snnNeuron_aie_integer<16>(in, out, threshold, 1, lineWidth);
-}
-
-//No difference with the one above due to how the neuron has been implemented
-void snnNeuronTileInteger(int32_t *in, int32_t *out, int32_t threshold,
-                  int32_t reset_value, int32_t tileHeight, int32_t tileWidth) {
-  snnNeuron_aie_integer<16>(in, out, threshold, reset_value, tileHeight, tileWidth);
-}
-
-} // extern "C"
