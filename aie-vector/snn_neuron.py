79faac1acb6198f01e606745b051338ff6ee6f18
Vittorio Palladino <vittorio.palladino@mail.polimi.it>
2025-04-11 19:42:23 +0100

    fixed name function

--- aie-vector/snn_neuron.py
+++ aie-vector/snn_neuron.py
@@ -8,7 +8,7 @@
 import numpy as np
 import sys
 
-from aie.iron import ObjectFifo, Program, Runtime, Worker
+from aie.iron import ObjectFifo, Program, Runtime, Worker, Kernel
 from aie.iron.placers import SequentialPlacer
 from aie.iron.device import NPU1Col1, NPU2
 from aie.iron.controlflow import range_
@@ -27,7 +27,7 @@ if len(sys.argv) > 1:
         raise ValueError("[ERROR] Device name {} is unknown".format(sys.argv[1]))
 
 
-def my_vector_bias_add():
+def snn_neuron():
     # Define tensor types
     aie_tile_ty = np.ndarray[(AIE_TILE_WIDTH,), np.dtype[np.int32]]
     all_data_ty = np.ndarray[(PROBLEM_SIZE,), np.dtype[np.int32]]
@@ -41,7 +41,8 @@ def my_vector_bias_add():
     of_in_spikes_0 = ObjectFifo(aie_tile_ty, name="in_spikes")
 
     # Object fifo for the input parameter (in this first implementation only the threshold is passed) No need of the ping pong buffer
-    of_in_threshold = ObjectFifo(threshold_ty, name="in_threshold", depth = 1)
+    # depth could be one in this case, no ping pong needed
+    of_in_threshold = ObjectFifo(threshold_ty, name="in_threshold")
 
     # object fifo between compute tiles and mem tiles
     of_out_spikes_0 = ObjectFifo(aie_tile_ty, name="out_threshold")
@@ -72,15 +73,15 @@ def my_vector_bias_add():
     rt = Runtime()
     with rt.sequence(all_data_ty, threshold_ty, all_data_ty) as (inTensor, inThreshold, outTensor):
         rt.start(worker)
-        rt.fill(of_input_spikes.prod(), inTensor)
+        rt.fill(of_input_spikes_0.prod(), inTensor)
         rt.fill(of_input_threshold, inThreshold)
-        rt.drain(of_output_spiks.cons(), outTensor, wait=True)
+        rt.drain(of_output_spikes_0.cons(), outTensor, wait=True)
 
     # Place program components (assign them resources on the device) and generate an MLIR module
     return Program(dev, rt).resolve_program(SequentialPlacer())
 
 
-module = my_vector_bias_add()
+module = snn_neuron()
 res = module.operation.verify()
 if res == True:
     print(module)
