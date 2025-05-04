0ec09f4cbba751daeaf403a7b95d01908bbc15a0
Vittorio Palladino <vittorio.palladino@mail.polimi.it>
2025-05-04 17:45:36 +0100

    class wrapper

---
+++ wrapping-library/snn_neuron_wrapper.py
@@ -0,0 +1,30 @@
+import numpy as np
+import subprocess
+from mlir_wrapper import compile_snn_neuron
+
+class snn_neuron_npu:
+    def __init__(self, in1_size=4096, out_size=4096, threshold=10, decay_factor=0.9, reset=-1, hard_reset=1, trace_size=8192, device='npu', use_placed=False):
+        self.in1_size = in1_size
+        self.out_size = out_size
+        self.threshold = threshold
+        self.decay_factor = decay_factor
+        self.reset = reset
+        self.hard_reset = hard_reset
+        self.trace_size = trace_size
+        self.device = device
+        self.use_placed = use_placed
+        self.compiled = False
+
+    def to_npu(self):
+        compile_snn_neuron(in1_size=self.in1_size, out_size=self.out_size, threshold=self.threshold, decay_factor= self.decay_factor, reset=self.reset, hard_reset=self.hard_reset, trace_size=self.trace_size, device=self.device, use_placed=self.use_placed)
+        self.compiled = True
+        print("Model Running on NPU...")
+
+    def __call__(self):
+        if self.compiled == False:
+            self.to_npu()
+        print("Running the testbench...")
+        subprocess.run(["make", "run"])
+        
+
+        
